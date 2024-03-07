6b1143d91f85f566d328198eee39abfd1bc888fd
Romain Ruetschi <106849+romac@users.noreply.github.com>
2024-03-07 19:08:18 +0100

    Fix path issue

--- src/serve.rs
+++ src/serve.rs
@@ -1,26 +1,35 @@
-use std::path::PathBuf;
+use std::path::Path;
 
 use axum::{
     body::Body,
-    extract::Path,
+    extract::Path as PathParam,
     http::{header::CONTENT_TYPE, StatusCode},
     response::Response,
 };
+use path_absolutize::Absolutize;
 
-pub async fn serve_file(dir: &str, Path(file): Path<String>) -> Response {
-    let path = PathBuf::from(dir).join(&file);
+pub async fn serve_file(dir: &str, PathParam(file): PathParam<String>) -> Response {
+    let file = Path::new(&file);
+
+    let Ok(path) = file.absolutize_virtually(dir) else {
+        return Response::builder()
+            .status(StatusCode::BAD_REQUEST)
+            .body(Body::from("Invalid file path"))
+            .unwrap();
+    };
+
     let mime_type = mime_guess::from_path(&path).first_or_text_plain();
 
     match tokio::fs::read(&path).await {
         Err(_) => Response::builder()
             .status(StatusCode::NOT_FOUND)
-            .body(Body::from(format!("File not found: {file}")))
+            .body(Body::from(format!("File not found: {}", file.display())))
             .unwrap(),
 
-        Ok(file) => Response::builder()
+        Ok(data) => Response::builder()
             .status(StatusCode::OK)
             .header(CONTENT_TYPE, mime_type.as_ref())
-            .body(Body::from(file))
+            .body(Body::from(data))
             .unwrap(),
     }
 }
